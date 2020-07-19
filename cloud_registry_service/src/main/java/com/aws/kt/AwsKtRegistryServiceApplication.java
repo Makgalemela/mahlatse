71a20ede37667793e14a2e3657f9137d6f3f229c
Akashkumar4 <akash.kumar@oodlestechnologies.com>
2020-07-19 13:40:39 +0530

    cloud_registry_service

---
+++ cloud_registry_service/src/main/java/com/aws/kt/AwsKtRegistryServiceApplication.java
@@ -0,0 +1,16 @@
+package com.aws.kt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+public class AwsKtRegistryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsKtRegistryServiceApplication.class, args);
+	}
+
+}
---
+++ cloud_registry_service/src/test/java/com/aws/kt/AwsKtRegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.kt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsKtRegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
